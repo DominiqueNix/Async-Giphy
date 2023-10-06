718606b9903ad039c7188d9a26a0a190148a4c1c
Shami Tomita <shamito@google.com>
2023-10-05 22:03:31 -0400

    gallery view

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ const apiKey = "uLJWnWHhVMFghFGEcbA9RcBKsUM19FGE"
 
 async function getImage(query){
 
-    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=1&offset=0&rating=g&lang=en`
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`
 
     try{
         const result = await fetch(endpoint);
         const data = await result.json();
-        console.log(data.data[0].embed_url);
-
+        console.log(data.data);
+        const array = data.data.map((object) => object.images.original.url);
+        console.log(array)
         const gif = data.data[0].images.original.url;
         //grabing the img and setting the src
-        let img = document.getElementById('gif');
-        img.setAttribute('src', gif)
-        console.log(img)
-
-        return gif;
+        let gallery = document.getElementById('gallery');
+        for(url of array){
+            const img = document.createElement('img');
+            img.src = url;
+            gallery.appendChild(img);
+        }
     }catch(error){
         console.log(error)
     }
